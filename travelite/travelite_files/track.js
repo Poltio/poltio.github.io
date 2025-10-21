1bfd229a482a9c4292b3d06c13c1c3758b30fced
cantugdonmez <cantugdonmez01@gmail.com>
2025-10-21 13:01:34 +0300

    create travelite demo

---
+++ travelite/travelite_files/track.js
@@ -0,0 +1,61 @@
+
+                    var el = document.createElement('script');
+                    el.setAttribute('type', 'text/javascript');
+                el.textContent = "\n \
+try{\n \
+    (function(){\n \
+        var containerId = 'b9fe1a7a6a';\n \
+        var uniqueDataLayerName = \"ntmData\"+Math.floor(Math.random()*10e12);\n \
+        window[uniqueDataLayerName] = [];\n \
+        window[uniqueDataLayerName].push({\n \
+            gdpr: \"-1\", /* 0 if GDPR does not apply, 1 if GDPR applies */\n \
+            gdprConsent: \"\", /* IAB TCF 2.0 consent string */\n \
+            pageType: 'homepage'\n \
+        });\n \
+        (function(n,e,o,r,y){n[r]=n[r]||[];n[r].push({'event':'ntmInit','t':new Date().getTime()});var f=e.getElementsByTagName(o)[0],s=e.createElement(o),d=r!='ntmData'?'&ntmData='+r:'';s.async=true;s.src='http'+(document.location.protocol=='https:'?'s':'')+'://tm.ad-srv.net/tm/a/container/init/'+y+'.js?'+d+'&rnd='+Math.floor(Math.random()*100000000);f.parentNode.insertBefore(s,f);})(window,document,'script',uniqueDataLayerName,containerId);\n \
+    })();\n \
+}catch(e){console.log(e)}\n \
+";document.body.appendChild(el);
+                var el = document.createElement('div');
+                el.setAttribute('style',  'height: 1px; width: 1px; top: 0px; left: 0px; position: absolute; overflow: hidden;');
+            el.innerHTML = '<iframe src=\"//pixel.bsmartdata.com/data.php?type=iframe&cid=ac_10411&loc=visit\" frameborder=\"0\" scrolling=\"no\" width=\"1\" height=\"1\" style=\"display:none;\"></iframe>';document.body.appendChild(el);
+                    var el = document.createElement('script');
+                    el.setAttribute('type', 'text/javascript');
+                el.textContent = "\n \
+var el = document.createElement('script'); el.setAttribute('type', 'text/javascript'); el.setAttribute('src', 'https://ad4m.at/network-tag.js'); el.onload = function() { advancedStoreNetworkTag.create({ advertiserId: \"10411\", networkId: \"4\", quantity: \"\", orderValue: \"\", orderId: \"\", productId: \"\", productCategory: \"\", rtOptout: \"\", pageType: \"startpage\" }); }; document.body.appendChild(el); \n \
+";document.body.appendChild(el);
+                    var el = document.createElement('script');
+                    el.setAttribute('type', 'text/javascript');
+                el.setAttribute('src', 'https://pix.hyj.mobi/rt?t=d&action=s&cid=10455');document.body.appendChild(el);
+                    var el = document.createElement('script');
+                    el.setAttribute('type', 'text/javascript');
+                el.textContent = " \n \
+  var tp_rtrgt_gdpr = '0';\n \
+  var tp_rtrgt_gdpr_consent = '';\n \
+  var tp_rtrgt_random = Math.random() * 10000000000000000;\n \
+  var tp_rtrgt_url = 'http'+(('https:'==document.location.protocol)?'s':'')+'://ad.ad-srv.net/retarget?a=68423&version=1';\n \
+  tp_rtrgt_url += '&gdpr=' + tp_rtrgt_gdpr;\n \
+  tp_rtrgt_url += '&gdpr_consent=' + tp_rtrgt_gdpr_consent;\n \
+  var tp_rtrgt_iframe = 'i' + tp_rtrgt_random;\n \
+  window.tp_rtrgt_iframes = window.tp_rtrgt_iframes||[];\n \
+  window.tp_rtrgt_iframes[tp_rtrgt_iframe] = document.createElement('iframe');\n \
+  window.tp_rtrgt_iframes[tp_rtrgt_iframe].height = \"1\";\n \
+  window.tp_rtrgt_iframes[tp_rtrgt_iframe].width = \"1\";\n \
+  window.tp_rtrgt_iframes[tp_rtrgt_iframe].scrolling = \"no\";\n \
+  window.tp_rtrgt_iframes[tp_rtrgt_iframe].cssText = \"position:absolute;visibility:hidden;left:0px;top:0px;width:1px;height:1px;border:0\";\n \
+  window.tp_rtrgt_iframes[tp_rtrgt_iframe].src = tp_rtrgt_url;\n \
+  var tp_rtrgt_first_script = document.getElementsByTagName('script')[0];\n \
+  tp_rtrgt_first_script.parentNode.appendChild( window.tp_rtrgt_iframes[tp_rtrgt_iframe] );\n \
+ ";document.body.appendChild(el);
+                    var el = document.createElement('script');
+                    el.setAttribute('type', 'text/javascript');
+                el.textContent = "\n \
+         var bavBounceManagement = {strAim : \n \
+\"https://travelite.lp.bounce-page.com/?product_id=\"};\n \
+";document.body.appendChild(el);
+                    var el = document.createElement('script');
+                    el.setAttribute('type', 'text/javascript');
+                el.setAttribute('src', 'https://api.bounce-commerce.de/bounce.min.js');document.body.appendChild(el);
+                    var el = document.createElement('script');
+                    el.setAttribute('type', 'text/javascript');
+                el.textContent = " if(window.localStorage && !localStorage.getItem(\"Conv_optOut\")){ var s=document.createElement(\"script\"); s.src=\"https://img.adnet.de/converify/adCode_63509.js\"; s.async=1; document.head.appendChild(s); } ";document.body.appendChild(el);
